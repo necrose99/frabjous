c3e106712dd4c503e53c3ff6686ef89ad0e733e7
csmk <csmk@chaoslab.org>
2017-01-11 09:54:54 +0900

    Travis: disable sudo again

--- .travis/install_portage.sh
+++ .travis/install_portage.sh
@@ -1,204 +1,67 @@
-#!/bin/bash -
+#!/bin/bash - 
 
 [ -z "${PORTAGE_ROOT}" ] && { echo "PORTAGE_ROOT not set"; exit 1; }
-mkdir -v -p "${PORTAGE_ROOT}"
+mkdir -p "${PORTAGE_ROOT}"
 [ -z "${PORTAGE_VER}" ] && { echo "PORTAGE_VER not set"; exit 1; }
-[ -z "${REPOMAN_VER}" ] && { echo "REPOMAN_VER not set"; exit 1; }
-: ${OVERLAY_NAME:=${TRAVIS_REPO_SLUG##*/}}
 
-# die on failure
 set -e
 
-source "$(dirname "${BASH_SOURCE[0]}")/travis-functions.sh" || exit 1
-
 get_archive() {
-	local url="${1?}" dir="${2?}"
+	local url="$1" dir="$2"
 	local tmpd="$(mktemp -d)" file="${url##*/}"
 
-	pushd "${tmpd}" >/dev/null
-
+	pushd "${tmpd}"
 	wget "$url" -O "${file}" || return 1
 	mkdir -p "${dir}"
 	tar xf "${file}" -C "${dir}" --strip-components=1 || return 2
-
-	popd >/dev/null
+	popd
 	rm -r -f "${tmpd}"
 }
 
 ## prepare env
 ## ------------
-fold_start environment "Prepare environment"
+tmp_dir="$(mktemp -d)"
+gentoo_tree_dir="${PORTAGE_ROOT}/usr/portage"
+portage_conf_dir="${PORTAGE_ROOT}/etc/portage"
+DISTDIR="$(mktemp -d)"
+mkdir -p "${PORTAGE_ROOT}/usr/lib64" && ln -s lib64 "${PORTAGE_ROOT}/usr/lib"
 
-PORTAGE_REPOS_DIR="${PORTAGE_ROOT}/usr/portage"   && mkdir -v -p "${PORTAGE_REPOS_DIR}"
-PORTAGE_CONF_DIR="${PORTAGE_ROOT}/etc/portage"  && mkdir -v -p "${PORTAGE_CONF_DIR}"
-DISTDIR="$(mktemp -d --suffix=-DISTDIR)"        && echo "DISTDIR: ${DISTDIR}"
+## install portage
+## ----------------
+get_archive https://github.com/gentoo/portage/archive/v${PORTAGE_VER}.tar.gz "${tmp_dir}/portage-src"
+cd "${tmp_dir}/portage-src"
+./setup.py install -O2 --system-prefix="${PORTAGE_ROOT}/usr" --sysconfdir="${PORTAGE_ROOT}/etc"
+mkdir -p "${PORTAGE_ROOT}/usr/lib/portage/cnf/"
+cp cnf/metadata.dtd "${DISTDIR}/"
 
-mkdir -v -p "${PORTAGE_ROOT}/usr/lib64"
-ln -v -s 'lib64' "${PORTAGE_ROOT}/usr/lib"
-
-fold_end environment
-
-
-run_module() {
-	local mod_name="$1"
-
-	local WORKDIR="$(mktemp -d --suffix=-WORKDIR)"
-	local S="${WORKDIR}/${mod_name}"
-
-	pushd "${WORKDIR}" >/dev/null
-
-	local phases=(
-		setup
-		fetch
-		prepare
-		configure
-		compile
-		preinst
-		install
-		postinst
-	)
-
-	(
-	fold_start "${mod_name}" "Running module '${mod_name}'"
-
-	for ph in "${phases[@]}" ; do
-		local fn="${mod_name}:${ph}"
-
-		[[ "${ph}" == prepare ]] && [ -d "${S}" ] && cd "${S}"
-
-		if declare -f "${fn}" >/dev/null ; then
-			fold_start "${fn//:/.}" "${ph^}"
-				"${fn}"
-			fold_end "${fn//:/.}"
-		fi
-	done
-
-	fold_end "${mod_name}"
-	)
-
-	popd >/dev/null
-	rm -rf "${WORKDIR}"
-}
-
-## BEGIN -- portage -----------------------------------------------------------
-
-portage:fetch() {
-	get_archive \
-		"https://github.com/gentoo/portage/archive/portage-${PORTAGE_VER}.tar.gz" \
-			"${S}"
-}
-
-portage:install() {
-	local args=(
-		-O2
-		--system-prefix="${PORTAGE_ROOT}/usr"
-		--sysconfdir="${PORTAGE_ROOT}/etc"
-	)
-	./setup.py install "${args[@]}"
-}
-
-## END -- portage -------------------------------------------------------------
-
-## BEGIN -- repoman -----------------------------------------------------------
-
-repoman:fetch() {
-	get_archive \
-		"https://github.com/gentoo/portage/archive/repoman-${REPOMAN_VER}.tar.gz" \
-			"${S}"
-	S+="/repoman"
-}
-
-repoman:prepare() {
-	:
-
-	# allow to have arbitrary copyright holder name and deprecate $Id$ header
-	sed -e 's|if num > 2:|if num >= 0:|' \
-		-i -- pym/repoman/modules/scan/ebuild/checks.py
-}
-
-repoman:install() {
-	local args=(
-		-O2
-		--system-prefix="${PORTAGE_ROOT}/usr"
-		--sysconfdir="${PORTAGE_ROOT}/etc"
-	)
-	./setup.py install "${args[@]}"
-}
-
-repoman:postinst() {
-	wget 'https://www.gentoo.org/dtd/metadata.dtd' -P "${DISTDIR}/"
-}
-
-## END -- repoman -------------------------------------------------------------
-
-## BEGIN -- repos -------------------------------------------------------------
-
-# declaration inside a function doesn't work
-declare -g -A REPOS=(
-	[gentoo]='https://github.com/gentoo-mirror/gentoo/archive/master.tar.gz'
-)
-
-repos:fetch() {
-	local r
-	for r in "${!REPOS[@]}" ; do
-		get_archive \
-			"${REPOS[${r}]}" \
-				"${S}/${r}"
-	done
-}
-
-repos:install() {
-	local r
-	for r in "${!REPOS[@]}" ; do
-		mv -v "${r}" "${PORTAGE_REPOS_DIR}/"
-	done
-}
-
-repos:postinst() {
-	mkdir -v -p "${PORTAGE_CONF_DIR}/repos.conf"
-
-	announce tee "${PORTAGE_CONF_DIR}/repos.conf/default" <<-_EOF_
-	[DEFAULT]
-	main-repo = gentoo
-_EOF_
-
-	announce tee "${PORTAGE_CONF_DIR}/repos.conf/${OVERLAY_NAME}" <<-_EOF_
-	[${OVERLAY_NAME}]
-	location = ${TRAVIS_BUILD_DIR}
-_EOF_
-
-	local r
-	for r in "${!REPOS[@]}" ; do
-		announce tee "${PORTAGE_CONF_DIR}/repos.conf/${r}" <<-_EOF_
-		[${r}]
-		location = ${PORTAGE_REPOS_DIR}/${r}
-_EOF_
-	done
-}
-
-## END -- repos ---------------------------------------------------------------
-
-run_module portage
-run_module repoman
-run_module repos
+## install gentoo tree
+## --------------------
+get_archive https://github.com/gentoo-mirror/gentoo/archive/master.tar.gz "${gentoo_tree_dir}"
 
 ## install portage configs
 ## ------------------------
-fold_start configuration "Configure"
+mkdir -p "${portage_conf_dir}/repos.conf"
+cat > "${portage_conf_dir}/repos.conf/repos" << _EOF_
+[DEFAULT]
+main-repo = gentoo
 
-announce tee "${PORTAGE_CONF_DIR}/make.conf" << _EOF_
+[gentoo]
+location = ${gentoo_tree_dir}
+
+[rindeal]
+location = ${TRAVIS_BUILD_DIR}
+_EOF_
+
+cat > "${portage_conf_dir}/make.conf" << _EOF_
 DISTDIR="${DISTDIR}"
-PKGDIR="$(mktemp -d --suffix=-PKGDIR)"
-PORTAGE_TMPDIR="$(mktemp -d --suffix=-PORTAGE_TMPDIR)"
-RPMDIR="$(mktemp -d --suffix=-RPMDIR)"
+PKGDIR="$(mktemp -d)"
+PORTAGE_TMPDIR="$(mktemp -d)"
+RPMDIR="$(mktemp -d)"
 _EOF_
 
-mkdir -p "${PORTAGE_CONF_DIR}/make.profile"
-announce tee "${PORTAGE_CONF_DIR}/make.profile/parent" << _EOF_
-gentoo:base
-gentoo:default/linux
-gentoo:targets/systemd
-rindeal:
-_EOF_
+ln -s "${gentoo_tree_dir}/profiles/base" "${portage_conf_dir}/make.profile"
 
-fold_end configuration
+## cleanup
+## --------
+rm -rf "${tmp_dir}"
+
